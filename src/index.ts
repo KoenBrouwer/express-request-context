ce1f7a225eb45d2d0fd179fe62820e4dbc213a89
Koen Brouwer <koen@openweb.nl>
2020-02-14 11:50:04 +0100

    Also adding context to the Response object.

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,19 @@ declare global {
 				[key: string]: any
 			}
 		}
+
+		interface Response {
+			context: {
+				[key: string]: any
+			}
+		}
 	}
 }
 
+/** Middleware that creates a new empty object on the Request and Response object */
 const expressContext = () => (req: Express.Request, res: Express.Response, next: NextFunction) => {
 	req.context = {};
+	res.context = {};
 	next();
 };
 
